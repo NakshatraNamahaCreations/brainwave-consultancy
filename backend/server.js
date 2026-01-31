4d1caab1ef424c4f358d01ae35c8aad132aa40c2
NakshatraNamahaCreations <ops.nnc@gmail.com>
2026-01-31 16:23:38 +0530

    finalized

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,15 @@ app.use(express.json());
    NODEMAILER TRANSPORT
 ================================ */
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 /* ===============================
    ENQUIRY FORM
